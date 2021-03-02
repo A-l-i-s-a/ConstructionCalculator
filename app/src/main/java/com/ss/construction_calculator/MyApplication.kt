40956fbea68b7442ad7aa0979189b06bee7fc5ff
Irina Ivlieva <ivlieva@haulmont.com>
2021-03-02 21:37:57 +0400

    Init commit

---
+++ app/src/main/java/com/ss/construction_calculator/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.ss.construction_calculator
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
